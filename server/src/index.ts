c807a0b9a020310d81270e05e05ae76bb4418479
Emir Yscn <emiryasacan21@gmail.com>
2025-04-05 22:01:16 +0300

    fix ratelimiter

--- server/src/index.ts
+++ server/src/index.ts
@@ -53,7 +53,7 @@ if (process.env.NODE_ENV === "development") {
 // Limit requests from same API
 if (process.env.NODE_ENV === "production") {
   const limiter = rateLimit({
-    max: 100,
+    max: 1000,
     windowMs: 60 * 60 * 1000,
     message: "Too many requests from this IP, please try again in an hour!",
   });
